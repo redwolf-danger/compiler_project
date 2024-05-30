69c4625feecf9f93d08f7a993cfbf994f740916a
redwolf-danger <soniadvaita@gmail.com>
2024-05-30 14:46:00 +0530

    testing if log statements work

--- src/App.jsx
+++ src/App.jsx
@@ -8,27 +8,7 @@ import axios from 'axios';
 function App() {
   // const [count, setCount] = useState(0)
   const handle = ()=>{
-    let data = qs.stringify({
-      'code': 'val = int(input("Enter your value: ")) + 5\nprint(val)',
-      'language': 'py',
-      'input': '7'
-  });
-  let config = {
-      method: 'post',
-      url: 'https://api.codex.jaagrav.in',
-      headers: {
-          'Content-Type': 'application/x-www-form-urlencoded'
-      },
-      data : data
-  };
-  axios(config)
-  .then(function (response) {
-    console.log("response was ", response);
-    console.log(JSON.stringify(response.data));
-  })
-  .catch(function (error) {
-    console.log(error);
-  });
+    console.log("btn clciked")
   }
   return (
     <button onClick={handle}>Click me for answer</button>
